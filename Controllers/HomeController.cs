c45084b30034109a57f2f48d78bb64aa083ca098
Monsteroooo <wmovchun2016@gmail.com>
2026-08-17 11:04:23 +0300

    Created Db. Added possibility to download note as markdown file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using NotesApp.Models;
+using NotesApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace NotesApp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        this._context = context;
     }
 
     public IActionResult Index()
@@ -27,5 +31,19 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet ("download/{id}")]
+    public async Task<IActionResult> DownloadNote(Guid id)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if (note == null)
+        {
+            return NotFound();
+        }
+
+        var bytes = System.Text.Encoding.UTF8.GetBytes(note.Content);
+
+        return File(bytes, "text/markdown", $"{note.Title}.md");
     }
 }
--- Hubs/NotesHub.cs
+++ Hubs/NotesHub.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.SignalR;
+using NotesApp.Data;
+using NotesApp.Models;
 
 namespace NotesApp.Hubs;
 
 public class NotesHub : Hub
 {
+    private readonly ApplicationDbContext context;
+
+    public NotesHub(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+
     public async Task SendNote(string note, string noteId)
     {
-        await Clients.Group(noteId).SendAsync("ReceiveNote", note);
+        var myNote = new Note
+        {
+            Id = Guid.NewGuid(),
+            Content = note,
+            Title = "New Note",
+            CreatedAt = DateTime.UtcNow
+        };
+
+        this.context.Notes.Add(myNote);
+        await this.context.SaveChangesAsync();
+        await Clients.Group(noteId).SendAsync("ReceiveNote", myNote);
     }
 
     public async Task JoinNoteGroup(string noteId)
